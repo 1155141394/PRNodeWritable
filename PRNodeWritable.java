12747a658672d1ac51fe0c68245b921706ab61e4
taozhisheng <1155141394@link.cuhk.edu.hk>
2022-11-20 22:56:18 +0800

    PRNodeWritable

--- PRNodeWritable.java
+++ PRNodeWritable.java
@@ -3,12 +3,14 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.*; // 引入 ArrayList 类
 import java.util.HashMap;
+import java.util.Arrays;
 
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.BooleanWritable;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
@@ -19,35 +21,28 @@ import org.apache.hadoop.classification.InterfaceStability;
 
 public class PRNodeWritable implements Writable {
     // Some data
-    private IntWritable distance = new IntWritable(Integer.MAX_VALUE);
-    private IntWritable prev = new IntWritable(0);
-    private MapWritable adjList = new MapWritable();
+    private FloatWritable distance = new FloatWritable(1);
+    private ArrayWritable adjList = new ArrayWritable();
 
     public BooleanWritable flag = new BooleanWritable(true);
 
-    public void PDNodeWritable() throws IOException {
-        this.distance = new IntWritable(Integer.MAX_VALUE);
-        this.prev = new IntWritable(0);
-        this.adjList = new MapWritable();
+    public void PRNodeWritable() throws IOException {
+        this.distance = new FloatWritable(1);
+        this.adjList = new ArrayWritable();
         this.flag = new BooleanWritable(true);
     }
 
-    public void set (IntWritable distance, IntWritable prev, MapWritable adjList, BooleanWritable flag){
+    public void set (FloatWritable distance, ArrayWritable adjList, BooleanWritable flag){
         this.distance = distance;
-        this.prev = prev;
         this.adjList = adjList;
         this.flag = flag;
     }
 
-    public void setDistance(IntWritable distance){
+    public void setDistance(FloatWritable distance){
         this.distance = distance;
     }
 
-    public void setPrev(IntWritable prev){
-        this.prev = prev;
-    }
-
-    public void setAdjList(MapWritable adjList){
+    public void setAdjList(ArrayWritable adjList){
         this.adjList = adjList;
     }
 
@@ -56,16 +51,12 @@ public class PRNodeWritable implements Writable {
     }
 
 
-    public IntWritable getDistance() {
+    public FloatWritable getDistance() {
         return this.distance;
     }
 
-    public MapWritable getAdjList() {
+    public ArrayWritable getAdjList() {
         return this.adjList;
-    }
-
-    public IntWritable getPrev(){
-        return this.prev;
     }
 
     public BooleanWritable getFlag() {
@@ -74,119 +65,107 @@ public class PRNodeWritable implements Writable {
 
     public void readFields(DataInput in) throws IOException {
         distance.readFields(in);
-        prev.readFields(in);
         adjList.readFields(in);
         flag.readFields(in);
     }
 
     public void write(DataOutput out) throws IOException {
         distance.write(out);
-        prev.write(out);
         adjList.write(out);
         flag.write(out);
     }
 
-    public static PDNodeWritable read(DataInput in) throws IOException {
-        PDNodeWritable pd = new PDNodeWritable();
-        pd.readFields(in);
-        return pd;
+    public static PRNodeWritable read(DataInput in) throws IOException {
+        PRNodeWritable pr = new PRNodeWritable();
+        pr.readFields(in);
+        return pr;
     }
 
     public String toString() {
         StringBuilder result = new StringBuilder();
-        IntWritable distance = this.distance;
-        IntWritable prev = this.prev;
-        MapWritable adjList = this.adjList;
+        FloatWritable distance = this.distance;
+        ArrayWritable adjList = this.adjList;
         BooleanWritable flag = this.flag;
+
+
+        Object[] objects = adjList.toArray();
+        long[] keys = new long[objects.length];
+        System.arraycopy(objects, 0, keys, 0, objects.length);
+
         String s = new String(" ");
-        Set<Writable> keys = adjList.keySet();
-        for (Writable key : keys) {
-            IntWritable count = (IntWritable) adjList.get(key);
-            s = s + key.toString() + ":" + count.toString() + "," ;
+        for (long key : keys) {
+            s = s + key.toString() + "," ;
         }
         s = s + " ";
-        result.append( distance.toString() + " " + prev.toString() + " " + flag.toString() + s );
+        result.append( distance.toString() + " " + flag.toString() + s );
         return result.toString();
 
     }
 
     public String toString(LongWritable nid) {
         StringBuilder result = new StringBuilder();
-        IntWritable distance = this.distance;
-        IntWritable prev = this.prev;
-        MapWritable adjList = this.adjList;
+        FloatWritable distance = this.distance;
+        ArrayWritable adjList = this.adjList;
         BooleanWritable flag = this.flag;
+
+        Object[] objects = adjList.toArray();
+        long[] keys = new long[objects.length];
+        System.arraycopy(objects, 0, keys, 0, objects.length);
+
         String s = new String(" ");
-        Set<Writable> keys = adjList.keySet();
-        for (Writable key : keys) {
-            IntWritable count = (IntWritable) adjList.get(key);
-            s = s + key.toString() + ":" + count.toString() + "," ;
+        for (long key : keys) {
+            s = s + key.toString() + "," ;
         }
         s = s + " ";
-        result.append( nid.toString() + "\t" + distance.toString() + " " + prev.toString() + " " + flag.toString() + s );
+        result.append( nid.toString() + "\t" + distance.toString() + " " + flag.toString() + s );
         return result.toString();
 
     }
 
-    public void copy(PDNodeWritable pd, LongWritable nid){
-        String pdStr = pd.toString(nid);
-        Text pdText = new Text();
-        pdText.set(pdStr);
-        this.getByText(pdText);
+    public void copy(PRNodeWritable pr, LongWritable nid){
+        String prStr = pr.toString(nid);
+        Text prText = new Text();
+        prText.set(prStr);
+        this.getByText(prText);
         return;
     }
 
 
-    public static Map<Integer,Integer> getStringToMap(String str){
+    public static LongWritable[] getStringToArray(String str){
         String[] str1 = str.split(",");
-        //创建Map对象
-        Map<Integer,Integer> map = new HashMap<>();
-        //循环加入map集合
+        LongWritable[] array = new LongWritable[str1.length];
+
         for (int i = 0; i < str1.length; i++) {
-            //根据":"截取字符串数组
-            String[] str2 = str1[i].split(":");
-            //str2[0]为KEY,str2[1]为值
-            int int1 = Integer.parseInt(str2[0]);
-            int int2 = Integer.parseInt(str2[1]);
-            map.put(int1,int2);
+            long l = Long.parseLong(str1[i]);
+            LongWritable longWritable = new LongWritable(l);
+            array[i] = longWritable;
         }
-        return map;
+        return array;
     }
 
 
     public int getByText(Text t){
-        PDNodeWritable node = new PDNodeWritable();
+        PRNodeWritable node = new PRNodeWritable();
         String str = t.toString();
         String[] all = str.trim().split(" ");
         String[] nodeAndDist = all[0].split("\t");
         int nid = Integer.parseInt(nodeAndDist[0]);
-        int distance = Integer.parseInt(nodeAndDist[1]);
-        IntWritable distanceWritable = new IntWritable(distance);
+        float distance = Float.parseFloat(nodeAndDist[1]);
+        FloatWritable distanceWritable = new FloatWritable(distance);
 
-        int prev = Integer.parseInt(all[1]);
-        IntWritable prevWritable = new IntWritable(prev);
-
-        boolean flag = Boolean.parseBoolean(all[2]);
+        boolean flag = Boolean.parseBoolean(all[1]);
         BooleanWritable flagWritable = new BooleanWritable(flag);
-        MapWritable mapWritable = new MapWritable();
-        if(all.length == 4)
+
+        ArrayWritable arrayWritable = new ArrayWritable();
+        if(all.length == 3)
         {
-            Map<Integer,Integer> map = getStringToMap(all[3]);
-
-
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                int key = entry.getKey();
-                int value = entry.getValue();
-                IntWritable keyWritable = new IntWritable(key);
-                IntWritable valueWritable = new IntWritable(value);
-                mapWritable.put(keyWritable, valueWritable);
-            }
+            LongWritable[] array = getStringToArray(all[2]);
+            arrayWritable.set(array);
         }
 
 
         this.distance = distanceWritable;
-        this.prev = prevWritable;
-        this.adjList = mapWritable;
+        this.adjList = arrayWritable;
         this.flag = flagWritable;
         return nid;
 

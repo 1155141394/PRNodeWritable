5434ba3c5644335cec582da040b961accfb86a36
TaoXU0 <464612404@qq.com>
2022-11-20 23:02:17 +0800

    Update PRPreProcess.java

--- PRPreProcess.java
+++ PRPreProcess.java
@@ -68,4 +68,5 @@ public class PRPreProcess {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }

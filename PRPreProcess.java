64e855fcf80c5b7120176ee4973f8eac44d3eea9
TaoXU0 <464612404@qq.com>
2022-11-20 23:28:07 +0800

    Update PRPreProcess.java

--- PRPreProcess.java
+++ PRPreProcess.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Mapper;
